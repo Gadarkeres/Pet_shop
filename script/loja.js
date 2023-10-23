510ca79ab165bb0103c3b58a2261fb6a9f655975
Matheus Rafael <matheusrafael77@hotmail.com>
2023-10-23 17:04:42 -0300

    update

--- script/loja.js
+++ script/loja.js
@@ -1,9 +1,27 @@
 const cart = document.querySelector('#cart')
 const close_cart = document.querySelector('#close-cart')
 const open_cart = document.querySelector('#card_open')
+const add_to_card = document.querySelectorAll('.button button');
+const remove_to_card = document.getElementsByClassName('remove-item')
+
 
 open_cart.addEventListener("click", menuisVisile);
 close_cart.addEventListener("click", menuisVisile)
+
+
+for(var i = 0; i < remove_to_card.length; i++){
+    remove_to_card[i].addEventListener('click', function(event){
+       event.target.parentElement.remove()
+        
+    })
+}
+
+
+for(var i = 0; i < add_to_card.length; i++){
+    add_to_card[i]
+}
+
+
 
 function menuisVisile() {
     if (cart.classList.contains('shopping-cart-hidden')){
