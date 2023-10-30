4531b95322d41e2174bc0d13051cce69c205cc18
Matheus Rafael <matheusrafael77@hotmail.com>
2023-10-30 15:05:34 -0300

    update

--- contato.js
+++ contato.js
@@ -1,23 +1,24 @@
 
-const alertButton = document.querySelector("#danger");
-const successButton = document.querySelector("#success");
+const alertButton = document.querySelector("#danger-form");
+const successButton = document.querySelector("#success-form ");
 const submitButton = document.querySelector('#submit');
 
 submitButton.addEventListener('click', function () {
-    alertButton.classList.remove("alert.active");
+    alertButton.style.display = 'none';
     const inputs = document.querySelectorAll('.input');
     const textarea = document.querySelector('textarea');
 
     const isAnyFieldEmpty = Array.from(inputs).some(input => input.value === '') || textarea.value === '';
 
     if (isAnyFieldEmpty) {
-        alertButton.classList.add("alert.active");
+        alertButton.style.display = 'none';
+        alertButton.style.display = 'block'
     } else {
         textarea.value = '';
         Array.from(inputs).forEach(input => (input.value = ''));
-        successButton.classList.add("alert.active");
+        successButton.style.display = 'block' 
         setTimeout(() => {
-            successButton.classList.remove("alert.active");
+            successButton.style.display = 'none';
         }, 2500);
     }
 });
