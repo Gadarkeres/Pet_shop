78cceda40153991d109284f8fb8854fcf25cc06d
Matheus Rafael <matheusrafael77@hotmail.com>
2023-10-30 15:11:51 -0300

    update

--- loja.js
+++ loja.js
@@ -12,10 +12,10 @@ const buy_Button = document.querySelector("#buy").addEventListener("click",final
     const value = document.querySelector(".total p").textContent;
     console.log(value);
     if (value === "TOTAL : R$0,00") {
-      error_Button.classList.add("alert.active");
+      error_Button.style.display = 'block'
       setTimeout(() => {
-        error_Button.classList.remove("alert.active");
-      }, 2500);
+        error_Button.style.display = 'none';
+      }, 2000);
     } else {
       alert("Obrigado pela compra, volte sempre!");
     }
@@ -43,9 +43,9 @@ function removeProduct(event) {
   }
   if (container) {
     container.remove();
-    alertButton.classList.add("alert.active");
+    alertButton.style.display = 'block';
     setTimeout(() => {
-      alertButton.classList.remove("alert.active");
+      alertButton.style.display = 'none';
     }, 2000);
     updateTotal();
   }
