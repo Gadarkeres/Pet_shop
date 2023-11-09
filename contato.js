523544582f314dd7d8b5fb2b0ec7c4adcdb3dd46
Matheus Rafael <matheusrafael77@hotmail.com>
2023-11-09 11:56:17 -0300

    update

--- contato.js
+++ contato.js
@@ -1,48 +1,49 @@
-
 const alertButton = document.querySelector("#danger-form");
 const successButton = document.querySelector("#success-form ");
-const submitButton = document.querySelector('#submit');
+const submitButton = document.querySelector("#submit");
 
-const error_name = document.querySelector('#error_name')
-const error_email = document.querySelector('#error_email')
-const error_msg = document.querySelector('#error_msg')
+const error_name = document.querySelector("#error_name");
+const error_email = document.querySelector("#error_email");
+const error_msg = document.querySelector("#error_msg");
 
-submitButton.addEventListener('click', function () {
-    Validate()
-    alertButton.style.display = 'none';
-    const inputs = document.querySelectorAll('.input');
-    const textarea = document.querySelector('textarea');
-    const isAnyFieldEmpty = Array.from(inputs).some(input => input.value === '') || textarea.value === '';
+submitButton.addEventListener("click", function () {
+  Validate();
+  alertButton.style.display = "none";
+  const inputs = document.querySelectorAll(".input");
+  const textarea = document.querySelector("textarea");
+  const isAnyFieldEmpty =
+    Array.from(inputs).some((input) => input.value === "") ||
+    textarea.value === "";
 
-    if (isAnyFieldEmpty) {
-        alertButton.style.display = 'none';
-        alertButton.style.display = 'block'
-    } else {
-        textarea.value = '';
-        Array.from(inputs).forEach(input => (input.value = ''));
-        successButton.style.display = 'block' 
-        setTimeout(() => {
-            successButton.style.display = 'none';
-        }, 2500);
-    }
+  if (isAnyFieldEmpty) {
+    alertButton.style.display = "none";
+    alertButton.style.display = "block";
+  } else {
+    textarea.value = "";
+    Array.from(inputs).forEach((input) => (input.value = ""));
+    successButton.style.display = "block";
+    setTimeout(() => {
+      successButton.style.display = "none";
+    }, 2500);
+  }
 });
 
 const Validate = () => {
-    error_name.style.display = 'none'
-    error_email.style.display = 'none'
-    error_msg.style.display = 'none'
-    // user
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const msg = document.querySelector('.textarea').value
+  error_name.style.display = "none";
+  error_email.style.display = "none";
+  error_msg.style.display = "none";
+  // user
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const msg = document.querySelector(".textarea").value;
 
-    if(name ===''){
-        error_name.style.display = 'initial'
-    }
-     if(email === ''){
-        error_email.style.display = 'initial'
-    }
- if(msg === ''){
-        error_msg.style.display = 'initial'
-    }
-}
+  if (name === "") {
+    error_name.style.display = "initial";
+  }
+  if (email === "") {
+    error_email.style.display = "initial";
+  }
+  if (msg === "") {
+    error_msg.style.display = "initial";
+  }
+};
