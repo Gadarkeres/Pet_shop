e808bdda1b5c94858c001c75caab94a77d142320
Matheus Rafael <matheusrafael77@hotmail.com>
2023-11-09 11:55:05 -0300

    update

--- contato.js
+++ contato.js
@@ -3,21 +3,15 @@ const alertButton = document.querySelector("#danger-form");
 const successButton = document.querySelector("#success-form ");
 const submitButton = document.querySelector('#submit');
 
-
 const error_name = document.querySelector('#error_name')
 const error_email = document.querySelector('#error_email')
 const error_msg = document.querySelector('#error_msg')
-
-
-
-
 
 submitButton.addEventListener('click', function () {
     Validate()
     alertButton.style.display = 'none';
     const inputs = document.querySelectorAll('.input');
     const textarea = document.querySelector('textarea');
-
     const isAnyFieldEmpty = Array.from(inputs).some(input => input.value === '') || textarea.value === '';
 
     if (isAnyFieldEmpty) {
@@ -41,7 +35,7 @@ const Validate = () => {
     const name = document.querySelector('#name').value
     const email = document.querySelector('#email').value
     const msg = document.querySelector('.textarea').value
-    
+
     if(name ===''){
         error_name.style.display = 'initial'
     }
